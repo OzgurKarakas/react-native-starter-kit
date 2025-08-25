5870556faeae9faca5de82c32a8dfb4345261cb4
Özgür Karakaş <ozgurkarakas025@gmail.com>
2025-08-25 22:13:59 +0300

    feat(query): setup TanStack Query provider and configuration

--- App.tsx
+++ App.tsx
@@ -10,22 +10,31 @@ import { View, Text, StatusBar } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { useTranslation } from 'react-i18next';
 import LanguageSelector from './src/components/common/LanguageSelector/LanguageSelector';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './src/services/queryClient';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
   const { t } = useTranslation();
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle="dark-content" />
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <StatusBar barStyle="dark-content" />
 
-      <SafeAreaView edges={['top']} className={'flex-row justify-end p-[10px]'}>
-        <LanguageSelector />
-      </SafeAreaView>
+        <SafeAreaView
+          edges={['top']}
+          className={'flex-row justify-end p-[10px]'}
+        >
+          <LanguageSelector />
+        </SafeAreaView>
 
-      <View className="flex-1 justify-center items-center">
-        <Text className="text-2xl ">{t('welcome')}</Text>
-      </View>
-    </SafeAreaProvider>
+        <View className="flex-1 justify-center items-center">
+          <Text className="text-2xl ">{t('welcome')}</Text>
+        </View>
+      </SafeAreaProvider>
+      {__DEV__ && <ReactQueryDevtools initialIsOpen={false} />}
+    </QueryClientProvider>
   );
 }
 
